afac8ed938ec62b91055d7d276ccaa20195c659a
nothasson <hassonL@foxmail.com>
2020-01-17 08:56:02 +0800

    something changes

---
+++ src/pages/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div>home</div>
+    <router-link to='./link'>List</router-link>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style>
+</style>
