4c009e7ce5fea88e9f7de6e9a8e11413a7ea5e22
nothasson <hassonL@foxmail.com>
2020-01-27 20:58:49 +0800

    add some configs

--- src/pages/Home/components/Header.vue
+++ src/pages/Home/components/Header.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="header">
-    <div class="header-left">返回</div>
-    <div class="header-input">内容</div>
-    <div class="header-right">城市</div>
+    <div class="header-left">
+      <span class="iconfont">&#xe624;</span>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont icon-arrow">&#xe61f;</span>
+    </div>
   </div>
 </template>
 
@@ -12,10 +19,12 @@ export default {
 };
 </script>
 <style lang ="stylus" scoped>
+@import '~styles/varibles.styl';
+
 .header {
   line-height: 0.86rem;
   display: flex;
-  background: #00bcd4;
+  background: $bgColor;
   color: #fff;
 
   .header-left {
@@ -31,12 +40,21 @@ export default {
     height: 0.64rem;
     margin-top: 0.12rem;
     margin-left: 0.2rem;
+    padding-left: 0.2rem;
+    color: grey;
+    text-align: left;
+    font-size: 0.3rem;
   }
 
   .header-right {
     width: 1.24rem;
     float: right;
     text-align: center;
+    font-size: 0.3rem;
+
+    .icon-arrow {
+      margin-left: -0.04rem;
+    }
   }
 }
 </style>
