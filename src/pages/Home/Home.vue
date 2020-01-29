4dfc777b2668dca69368805c381fee55ec79dc57
nothasson <hassonL@foxmail.com>
2020-01-29 14:38:34 +0800

    add something

--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -3,19 +3,21 @@
     <home-header></home-header>
     <home-swiper></home-swiper>
     <home-icons></home-icons>
+    <home-recommend></home-recommend>
   </div>
 </template>
 <script>
 import HomeHeader from "./components/Header";
 import HomeSwiper from "./components/swiper";
 import HomeIcons from "./components/icons";
-
+import HomeRecommend from "./components/recommend";
 export default {
   name: "Home",
   components: {
     HomeHeader,
     HomeSwiper,
-    HomeIcons
+    HomeIcons,
+    HomeRecommend
   }
 };
 </script>
---
+++ src/pages/Home/components/recommend.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div class="recommend-title">热销推荐</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeRecommend"
+};
+</script>
+
+<style lang="stylus" scoped></style>
