8e1597f586818c4bc03bc9badca3c8e31c35b90c
nothasson <hassonL@foxmail.com>
2020-01-29 15:42:29 +0800

    增加推荐页

--- src/pages/Home/components/recommend.vue
+++ src/pages/Home/components/recommend.vue
@@ -1,13 +1,111 @@
 <template>
   <div>
-    <div class="recommend-title">热销推荐</div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <img
+          class="item-img"
+          :src="item.imgUrl"
+        />
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.content}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: "HomeRecommend"
+  name: "HomeRecommend",
+  data() {
+    return {
+      recommendList: [
+        {
+          id: "0001",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_200x200_2039e660.jpg",
+          title: "上海迪士尼乐园",
+          content: "上海市浦东新区川沙新镇上海迪士尼度假区内"
+        },
+        {
+          id: "0002",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/p0/1502/ba/ba5f873a22327e0f.water.jpg_200x200_9bf5cc14.jpg",
+          title: "上海影视乐园",
+          content: "上海市松江区车墩镇北松公路4915号"
+        },
+        {
+          id: "0003",
+          imgUrl:
+            "http://img1.qunarzz.com/tuan/team2/1507/2c/83e0e0e7ae082a.jpg_110x110_24c155d5.jpg",
+          title: "东方明珠",
+          content: "上海市浦东新区世纪大道1号"
+        },
+        {
+          id: 2,
+          imgUrl:
+            "http://img1.qunarzz.com/sight/p0/1603/17/170481fb9caf500790.water.jpg_200x200_a32b9a92.jpg",
+          title: "上海野生动物园",
+          content: "上海浦东南汇区南六公路178号"
+        }
+      ]
+    };
+  }
 };
 </script>
 
-<style lang="stylus" scoped></style>
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl';
+
+.title {
+  line-height: 0.8rem;
+  background: #eee;
+  text-indent: 0.2rem;
+  margin-top: 0.2rem;
+  font-size: 0.3rem;
+  text-align: left;
+}
+
+.item {
+  display: flex;
+  height: 1.9rem;
+  overflow: hidden;
+  text-align: left;
+
+  .item-img {
+    width: 1.7rem;
+    height: 1.7rem;
+    padding: 0.1rem;
+  }
+
+  .item-info {
+    flex: 1;
+    padding: 0.1rem;
+    min-width: 0;
+
+    .item-title {
+      line-height: 0.52rem;
+      font-size: 0.32rem;
+      ellipsis();
+    }
+
+    .item-desc {
+      line-height: 0.4rem;
+      color: #ccc;
+      ellipsis();
+    }
+
+    .item-button {
+      background: #ff9300;
+      padding: 0 0.2rem;
+      border-radius: 0.06rem;
+      margin-top: 0.16rem;
+      color: white;
+      line-height: 0.44rem;
+    }
+  }
+}
+</style>
---
+++ src/pages/Home/components/weekend.vue
@@ -0,0 +1,99 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of weekendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl" />
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.content}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeWeekend",
+  data() {
+    return {
+      weekendList: [
+        {
+          id: "0001",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/source/1505/7d/df6ff468331c43.jpg_r_640x214_1f3783d0.jpg",
+          title: "上海必游top10",
+          content: "中西合璧，现代和传统各有各的精彩"
+        },
+        {
+          id: "0002",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/source/1511/1d/fe098e8cfec117.jpg_r_640x214_a9be292a.jpg",
+          title: "上海城市观光",
+          content: "来这里寻找你所认识的上海"
+        },
+        {
+          id: "0003",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/source/1505/7b/2a6b0ce087f0e5.jpg_r_640x214_5c76b115.jpg",
+          title: "情迷博物馆",
+          content: "穿梭时光岁月寻找答案"
+        },
+        {
+          id: 4,
+          imgUrl:
+            "http://img1.qunarzz.com/sight/source/1505/b7/2f4621a3a4e12c.jpg_r_640x214_d55c10ba.jpg",
+          title: "学生最爱TOP10",
+          content: "学习之余最爱玩耍，爱玩的你不可错过"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl';
+
+.title {
+  line-height: 0.8rem;
+  background: #eee;
+  text-indent: 0.2rem;
+  margin-top: 0.2rem;
+  font-size: 0.3rem;
+  text-align: left;
+}
+
+.item {
+  text-align: left;
+
+  .item-img-wrapper {
+    height: 0;
+    overflow: hidden;
+    padding-bottom: 33.9%;
+
+    .item-img {
+      width: 100%;
+    }
+  }
+
+  .item-info {
+    padding: 0.1rem;
+
+    .item-title {
+      line-height: 0.52rem;
+      font-size: 0.32rem;
+      ellipsis();
+    }
+
+    .item-desc {
+      line-height: 0.4rem;
+      color: #ccc;
+      ellipsis();
+    }
+  }
+}
+</style>
